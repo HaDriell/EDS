3569e362ac23b7d9cdd88e3754a7249c99cc4673
Gautier Lathuiliere <glathuilie@univ-lyon2.fr>
2019-11-15 18:31:59 +0100

    [BROKEN] Inventory System & Basic Camera Controller

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+
+    public Item item;
+     
+    public override void OnInteract()
+    {
+        base.OnInteract();
+    }
+
+    void PickUp()
+    {
+        if (Inventory.instance.HasSpace(item))
+        {
+            Inventory.instance.Add(item);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TPSCameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPSCameraController : MonoBehaviour
+{
+    //TODO : finish this first ?
+    public Transform target;
+    public Vector3 offset;
+
+    public float pitch = 2;
+
+    void LateUpdate()
+    {
+        transform.position = target.position - offset;
+        transform.LookAt(target.position + Vector3.up * pitch);
+    }
+}
